b7f4d909521c7e54eb2b41049778e039f9e3983f
IanAlfaroSantos <ialfasa2020@gmail.com>
2025-04-08 19:37:42 -0600

    Arreglo de confirmación de contraseña

--- src/shared/validators/validateConfirPassword.jsx
+++ src/shared/validators/validateConfirPassword.jsx
@@ -1,6 +1,6 @@
 
 export const validateConfirPassword = (pass, confPass) => {
-    return pass = confPass;
+    return pass === confPass;
 }
 
 export const passwordConfirmationMessage = 'Las contraseñas no coinciden';
